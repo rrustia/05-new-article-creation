5cd5132de54f15b7406c5bdd56196fd065b25a96
Ronel Rustia <fedstats@yahoo.com>
2017-02-12 23:53:30 -0800

    still buggy

--- scripts/article.js
+++ scripts/article.js
@@ -23,13 +23,15 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
+function preventRawDataProblem() {
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
+}
 
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
